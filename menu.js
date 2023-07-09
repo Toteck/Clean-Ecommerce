76086dfa623fd8c1dd134a6f09e5e61bd5d8b41a
Toteck <53489827+Toteck@users.noreply.github.com>
2023-07-09 20:10:33 +0000

    style: creating a dropdown menu for products

--- menu.js
+++ menu.js
@@ -1,4 +1,3 @@
-
 let show = true;
 
 const menuSection = document.querySelector(".menu-section");
@@ -13,3 +12,36 @@ menuToggle.addEventListener("click", () => {
   
   show = !show
 })
+
+let show2 = true;
+const menuSectionOptions = menuSection.querySelector(".menu-section-options");
+const menuToggleProducts = menuSectionOptions.querySelector(".products-dropdown")
+const productsContent = menuToggleProducts.querySelector(".products-content")
+
+
+
+menuToggleProducts.addEventListener("click", () => {
+  if (productsContent.style.display === "flex") {
+    productsContent.style.display = "none";
+  } else {
+    productsContent.style.display = "flex";
+  }
+})
+
+// toggleMenuProducts() {
+//   let menu = document.getElementById("products-dropdown");
+//   if (x.style.display === "flex") {
+//     menu.style.display = "none"
+//   } else {
+//     menu.style.display = "flex";
+//   }
+// }
+
+// function myFunction() {
+//   var x = document.getElementById("myLinks");
+//   if (x.style.display === "block") {
+//     x.style.display = "none";
+//   } else {
+//     x.style.display = "block";
+//   }
+// }
